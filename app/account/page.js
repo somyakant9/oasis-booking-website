cdaa36f8ce6d94ab194841ea78adaa969e9e347b
somyakant9 <msomya143@gmail.com>
2024-06-19 17:57:02 +0530

    added different pages with styling

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
 
-
+export const metadata = {
+  title:"Guest area"
+}
 export default function Page() {
   return (
-    <div>Account Page</div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+       Welcome, SAM
+      </h2>
   )
 }
